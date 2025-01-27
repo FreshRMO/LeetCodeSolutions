a3fe5cb05d3844473cd3925340cd5d24b8231f8c
B Str <ben.strater@gmail.com>
2025-01-27 12:50:13 -0700

    adds problem 134 and solution

---
+++ 150LeetCodeProblemsInterview/134_Gas_station/Solution.ts
@@ -0,0 +1,19 @@
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+    const len: number = gas.length;
+
+    let startIndex: number = 0;
+    let totalSurplus: number = 0;
+
+    for (let i = 0, currentSurplus = 0; i < len; ++i) {
+        const surplus: number = gas[i] - cost[i];
+
+        totalSurplus += surplus;
+        currentSurplus += surplus;
+
+        if (currentSurplus < 0) {
+            startIndex = i + 1;
+            currentSurplus = 0;
+        }
+    }
+
+    return totalSurplus >= 0 ? startIndex : -1;
